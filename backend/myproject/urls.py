83dd723981e7f8457779dea41684e0e396a43dff
Divyansh <divyanshburman363@gmail.com>
2026-03-01 12:17:56 +0530

    Removed login_view import

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 from django.urls import path
 from .views import save_login_data
-from .views import login_view
+# from .views import login_view
 from .views import get_product
 from .views import product_list, product_detail
 
 urlpatterns = [
     path("api/login/", save_login_data, name="save_login_data"),
-    path("login/", login_view),
+    # path("login/", login_view),
     path("product/<int:ip>/", get_product),
     path('products/', product_list),
     path('products/<int:pk>/', product_detail),
